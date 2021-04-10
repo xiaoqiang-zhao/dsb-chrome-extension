86ac0f02cbdb4393228de1cfee2042c15df6d75f
xiaoqiang-zhao <longze_xq@163.com>
2021-04-10 14:53:46 +0800

    登陆后第一屏自动化

---
+++ contentScrips/etaxSichuanDesk.js
@@ -0,0 +1,20 @@
+/**
+ * @file 四川税务局 / 桌面(登录后的第一屏)
+ */
+// 间隔时间 800 毫秒
+const spaceTime = 3000;
+
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+        $('.knowBtn').click();
+    }, spaceTime);
+    setTimeout(() => {
+        $('.tabList').eq(1).click();
+    }, spaceTime * 2);
+    setTimeout(() => {
+        debugger
+        const linkDom = $('[data-title="税费申报及缴纳"]');
+        const link = linkDom.attr('data-url');
+        window.location.href = 'https://etax.sichuan.chinatax.gov.cn/' + link;
+    }, spaceTime * 3);
+});
